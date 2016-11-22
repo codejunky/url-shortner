09d810deab57ff2734ad479c8d8a9dcbb8418cd9
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-22 22:58:57 +0100

    Add functionality to shorten urls provided by users and to redirect the user to original URL when he provides the shorten one

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-var express = require('express');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-var app = express();
+const Url = require('./models/Url');
+
+const app = express();
 
 mongoose.connect('mongodb://fcc:Password1!@ds161487.mlab.com:61487/url-shortner');
 
-app.get('/', (req, res) => {
-    res.json({
-      message: "Hello world"
+app.get('/new/:url(*)', (req, res) => {
+  const url = req.params.url;
+  const host = req.headers['host'];
+
+  if (validator.isURL(url)) {
+    const short = Math.random().toString(36).slice(-5);
+    const newURL = new Url({original_url: url, short_url: short});
+
+    newURL.save((err, u) => {
+      return res.json({
+        original_url: u.original_url,
+        short_url: `${host}/${u.short_url}`
+      });
     });
+  } else {
+    return res.json({error: "URL invalid"});
+  }
+});
+
+app.get('/:short', (req, res) => {
+  const short = req.params.short;
+
+  Url.findOne({short_url: short}, (err, url) => {
+    if (err || !url) {
+      res.json({error: 'Short url not found or invalid'});
+    } else {
+      return res.redirect(url.original_url);
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
