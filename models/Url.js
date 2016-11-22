be0e19d37c339f4a871e1a2ebae43d572b00ba03
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-22 22:58:08 +0100

    Create Url model to store the urls and their shortend form

---
+++ models/Url.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const UrlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: String,
+    required: true
+  }
+});
+
+
+module.exports = mongoose.model('Url', UrlSchema);
